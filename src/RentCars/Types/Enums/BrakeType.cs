4c5380d0d0a49d5f7a0d0ff65cac0474d51aee17
gabriel <gabriel@ezlink.global>
2023-07-14 14:42:36 -0300

    req5

--- src/RentCars/Types/Enums/BrakeType.cs
+++ src/RentCars/Types/Enums/BrakeType.cs
@@ -6,8 +6,4 @@ public enum BrakeType
     Chamber = 1,
     Disc = 2,
     Drum = 3,
-    // O enum deve conter o campo Chamber com valor 1
-    // O enum deve conter o campo Disc com valor 2
-    // O enum deve conter o campo Drum com valor 3
-
 }
