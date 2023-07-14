2558bf78250f7a7cd6dec1f348bf1400148f6e76
gabriel <gabriel@ezlink.global>
2023-07-14 14:39:49 -0300

    req3

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -4,4 +4,23 @@ namespace RentCars.Models;
 //3 - Crie a classe `Vehicle`
 public class Vehicle
 {
+    public string Brand {get; set;} = "";
+    public string Model {get; set;} = "";
+    public decimal Price {get; set;}
+    public FuelType Fuel {get; set;}
+    public int EngineCapacity {get; set;}
+    public Color MainColor {get; set;}
+    public int Year {get; set;}
+    public double PricePerDay {get; set;}
+    public bool IsRented {get; set;} 
+    // Brand que deve ser do tipo string, ter valor padrão igual à "", deve ter get e set;
+    // Model que deve ser do tipo string, ter valor padrão igual à "", deve ter get e set;
+    // Price que deve ser do tipo decimal, deve ter get e set;
+    // Fuel que deve ser do tipo FuelType, deve ter get e set;
+    // EngineCapacity que deve ser do tipo int, deve ter get e set;
+    // MainColor que deve ser do tipo Color, deve ter get e set;
+    // Year que deve ser do tipo int, deve ter get e set;
+    // PricePerDay que deve ser do tipo double, deve ter get e set;
+    // IsRented que deve ser do tipo bool, ter valor padrão igual à false, deve ter get e set;
+ 
 }
