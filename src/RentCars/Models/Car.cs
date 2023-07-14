afa0cbcb9b43d6f891ff47b44b0ce76f16052154
gabriel <gabriel@ezlink.global>
2023-07-14 14:53:13 -0300

    req6

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -2,6 +2,17 @@ using RentCars.Types;
 namespace RentCars.Models;
 
 //6 - Faça a classe `Car` herdar de `Vehicle`
-public class Car
+public class Car : Vehicle
 {
+    public int Seats {get; set;}
+    public int Doors {get; set;}
+    public TractionType Traction {get; set;}
+    public BrakeType FrontBrake {get; set;}
+    public BrakeType RearBrake {get; set;}
+    // Seats que deve ser do tipo Int;
+    // Doors que deve ser do tipo Int;
+    // Traction que deve ser do tipo TractionType;
+    // FrontBrake que deve ser do tipo BrakeType;
+    // RearBrake que deve ser do tipo BrakeType;
+
 }
