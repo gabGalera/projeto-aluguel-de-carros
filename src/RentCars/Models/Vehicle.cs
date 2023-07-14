2eb17cd6b639de497a3165849e5e4e7cd102e927
gabriel <gabriel@ezlink.global>
2023-07-14 14:41:13 -0300

    req4

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -12,15 +12,5 @@ public class Vehicle
     public Color MainColor {get; set;}
     public int Year {get; set;}
     public double PricePerDay {get; set;}
-    public bool IsRented {get; set;} 
-    // Brand que deve ser do tipo string, ter valor padrão igual à "", deve ter get e set;
-    // Model que deve ser do tipo string, ter valor padrão igual à "", deve ter get e set;
-    // Price que deve ser do tipo decimal, deve ter get e set;
-    // Fuel que deve ser do tipo FuelType, deve ter get e set;
-    // EngineCapacity que deve ser do tipo int, deve ter get e set;
-    // MainColor que deve ser do tipo Color, deve ter get e set;
-    // Year que deve ser do tipo int, deve ter get e set;
-    // PricePerDay que deve ser do tipo double, deve ter get e set;
-    // IsRented que deve ser do tipo bool, ter valor padrão igual à false, deve ter get e set;
- 
+    public bool IsRented {get; set;}  
 }
--- src/RentCars/Types/Enums/BrakeType.cs
+++ src/RentCars/Types/Enums/BrakeType.cs
@@ -3,5 +3,11 @@ namespace RentCars.Types;
 //4 - Crie um enum `BrakeType`
 public enum BrakeType
 {
+    Chamber = 1,
+    Disc = 2,
+    Drum = 3,
+    // O enum deve conter o campo Chamber com valor 1
+    // O enum deve conter o campo Disc com valor 2
+    // O enum deve conter o campo Drum com valor 3
 
 }
